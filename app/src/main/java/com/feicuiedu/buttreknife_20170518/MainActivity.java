fcb45f2cf334f156de6696a920d174466df949cc
gqq <gqq519@163.com>
2017-05-18 15:43:53 +0800

    Butterknife的列表绑定、监听绑定和重置

--- app/src/main/java/com/feicuiedu/buttreknife_20170518/MainActivity.java
+++ app/src/main/java/com/feicuiedu/buttreknife_20170518/MainActivity.java
@@ -1,5 +1,6 @@
 package com.feicuiedu.buttreknife_20170518;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +8,18 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 import butterknife.BindColor;
 import butterknife.BindDimen;
 import butterknife.BindString;
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnItemLongClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     @BindDimen(R.dimen.activity_horizontal_margin)
     float dimen;
 
+    // 绑定视图列表
+    @BindViews({R.id.tv1,R.id.tv2,R.id.tv3})
+    List<TextView> mViewList;
+    private Unbinder mUnbinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,20 +51,70 @@ public class MainActivity extends AppCompatActivity {
         // 代码放大：双击shift-->搜索font-->Mouse 打开
         // ctrl+p：查看方法的参数
         // ctrl+alt+空格：提示
-        ButterKnife.bind(this);
+        // ctrl+alt+V 提取变量
+        // ctrl+alt+F 提取为成员变量
+        mUnbinder = ButterKnife.bind(this);
 
         // 绑定资源
 //        final int color = getResources().getColor(R.color.colorAccent);
 //        String string = getResources().getString(R.string.app_name);
 
         // 设置监听
-        mButton.setOnClickListener(new View.OnClickListener() {
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                mTextView.setText(appName);
+//                mTextView.setTextColor(color);
+//            }
+//        });
 
-            @Override
-            public void onClick(View v) {
-                mTextView.setText(appName);
-                mTextView.setTextColor(color);
-            }
-        });
+        // 间接绑定
+//        View view = ;
+//        ButterKnife.findById(view,R.id.zzz);
+
+        // 对视图列表进行操作
+        ButterKnife.apply(mViewList,mAction);
+    }
+
+    // 创建Action，泛型就是视图列表的视图类型
+    private ButterKnife.Action<TextView> mAction = new ButterKnife.Action<TextView>() {
+        @Override
+        public void apply(@NonNull TextView view, int index) {
+            view.setText("视图列表的文本:"+index);
+        }
+    };
+
+//    @OnClick
+//    @OnItemClick
+//    @OnItemLongClick
+
+    // 设置监听
+//    @OnClick(R.id.btnShow)
+//    public void onClick(View view){
+//        mTextView.setText(appName);
+//        mTextView.setTextColor(color);
+//    }
+
+    @OnClick({R.id.btnShow,R.id.btnShow1,R.id.btnShow2})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnShow:
+                Toast.makeText(this, "展示1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnShow1:
+                Toast.makeText(this, "展示2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnShow2:
+                Toast.makeText(this, "展示3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 解绑的操作
+        mUnbinder.unbind();
     }
 }
