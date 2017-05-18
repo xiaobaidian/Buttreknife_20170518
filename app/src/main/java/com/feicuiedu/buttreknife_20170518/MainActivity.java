9bf9003c4bc9e3162a216ed14c68c5a99f004e37
gqq <gqq519@163.com>
2017-05-18 14:09:24 +0800

    文档加入

---
+++ app/src/main/java/com/feicuiedu/buttreknife_20170518/MainActivity.java
@@ -0,0 +1,13 @@
+package com.feicuiedu.buttreknife_20170518;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
